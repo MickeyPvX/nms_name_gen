94e4dec9c270bf38b96b89659eb049614c0ce5d2
Michael Paul <mickeypvx@gmail.com>
2019-08-25 13:50:35 -0500

    add start class maps

---
+++ utils/star_class_map.py
@@ -0,0 +1,27 @@
+spectral_class_map = {
+    'O': ['Horus', 'Vishnu', 'Hera', 'Lir', 'Njord'],
+    'B': ['Hathor', 'Shiva', 'Athena', 'Boann', 'Frigg'],
+    'A': ['Ptah', 'Saraswati', 'Apollo', 'Epona', 'Heimdall'],
+    'F': ['Khons', 'Krishna', 'Hermes', 'Cernunnos', 'Baldur'],
+    'G': ['Ra', 'Ganesha', 'Aphrodite', 'Lugh', 'Mimir'],
+    'K': ['Bast', 'Agni', 'Hephaestus', 'Teutates', 'Tyr'],
+    'M': ['Sekhmet', 'Durga', 'Hestia', 'Aodh', 'Thor'],
+    'L': ['Thoth', 'Nakul', 'Ares', 'Morrigan', 'Sigyn'],
+    'T': ['Amon', 'Parvati', 'Poseidon', 'Druantia', 'Nerthus'],
+    'Y': ['Seth', 'Kali', 'Zeus', 'Anu', 'Audhumla'],
+    'E': ['Isis', 'Matangi', 'Demeter', 'Ce-aehd', 'Sif']
+}
+
+oddity_map = {
+    'E': 'Ei',
+    'F': 'Ef',
+    'H': 'Ah',
+    'K': 'Ak',
+    'M': 'Im',
+    'N': 'In',
+    'P': 'Op',
+    'Q': 'Oq',
+    'S': 'Us',
+    'V': 'Yv',
+    'W': 'Uw'
+}
